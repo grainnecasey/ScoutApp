b47d944ac025bd2442bee31977b04da0bf1d6844
Charlie Kenny <CharlesKenny19@bghs.org>
2016-11-10 20:47:26 -0500

    LOLOLOLOLOLOLOLOLOLOLOLOLOLOL

---
+++ app/src/main/java/com/example/grainne/scout/MatchConfirmation.java
@@ -0,0 +1,43 @@
+package com.example.grainne.scout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MatchConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_confirmation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    public void mainMenOnClick(View v) {
+        Intent i = new Intent(MatchConfirmation.this, StartScreen.class);
+        startActivity(i);
+    }
+
+    public void strongMenOnClick(View v) {
+        Intent i = new Intent(MatchConfirmation.this, StrongholdMenu.class);
+        startActivity(i);
+    }
+
+}
+
+}
